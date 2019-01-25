328e9a37fbb55be8823cbb158b4a5bd6a30826e4
Arttu mahlakaarto <arttu.mahlakaarto@gmail.com>
2019-01-25 05:47:26 +0200

    Create celery-rn.sh

---
+++ celery-rn.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Replacing strings"
+TARGET=/usr/local/lib/python3.7/site-packages/celery/backends
+if [ -d "$TARGET" ]; then
+echo "CD into target"
+cd $TARGET
+else
+echo "Finding celery..."
+celery_root="$(dirname "$(which celery)" )"
+cd $celery_root/../lib/python3.7/site-packages/celery/backends
+fi
+if [ -e async.py ]
+then
+    echo "editing usr"
+    mv async.py asynchronous.py
+    if [ -e redis.py ]; then
+    sed -i 's/async/asynchronous/g' redis.py
+    echo "REDIS"
+    fi
+    if [ -e rpc.py ]; then
+    sed -i 's/async/asynchronous/g' rpc.py
+    echo "RPC"
+    fi
+    echo "DONE"
+fi
